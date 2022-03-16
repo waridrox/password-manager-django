0df7a94c45b0387082224e2381d542912eea7bd7
Mohammad Warid <mohdwarid4@gmail.com>
2022-03-16 22:13:14 +0530

    Updated secrets with env variables

--- django_passman/settings.py
+++ django_passman/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from decouple import config
 from pathlib import Path
 import os
 
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.environ("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -140,7 +139,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
-KEY = config('KEY')
+KEY = os.environ('KEY')
 
 TAILWIND_APP_NAME = 'theme'
 
